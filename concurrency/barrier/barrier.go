d2254a926ce374da711d98641721cf35e3088f47
flyindoji <flyingdoji@protonmail.com>
2020-04-23 12:47:17 +0300

    Adapted barrier for multiple uses

--- concurrency/barrier/barrier.go
+++ concurrency/barrier/barrier.go
@@ -44,43 +44,58 @@ func newSemaphore(capacity int) *semaphore {
 // The part before rendezvous can be executed in any order
 // No thread may proceed to the critical part until all threads have reached the rendezvous
 
-func worker(id int, waitCount *int, mutex *semaphore, barrier *semaphore) {
+func worker(id, threadCount int, waitCount *int, mutex *semaphore, barrier, barrier2 *semaphore) {
 
-	println(id, "rendezvous")
-	mutex.Wait()
-	*waitCount--
-	// Nth thread to finish can signal the barrier and wake the n-1 thread
-	if *waitCount == 0 {
+	for {
+		println(id, "rendezvous")
+		mutex.Wait()
+		*waitCount--
+		// Nth thread unlocks the first barrier and locks the 2nd
+		if *waitCount == 0 {
+			barrier2.Wait()
+			barrier.Signal()
+		}
+		mutex.Signal()
+
+		// Block here until everyone reached rendezvous and thread N signals the barrier
+		barrier.Wait()
+		// Thread N-i unlocks the barrier by decrementing the semaphore to -1 and waking up the N-i-1 thread.
+		// Thread N-i-1 will increment the barrier to 0 and signal the N-2 thread, until all threads pass.
+		// Note: Thread N will call Signal() two times, this will cause the semaphore to be decremented one extra
+		// The barrier will not reset after all threads have executed the critical part
+		// This potentially allows a thread to loop around several times, effectively getting ahead of the others
 		barrier.Signal()
+		println(id, "critical after")
+
+		// Barrier reset logic here
+		// Requires a second barrier that blocks all threads until the first barrier is reset
+		mutex.Wait()
+		*waitCount++
+		// Nth thread locks the first barrier and unlocks the 2nd
+		if *waitCount == threadCount {
+			barrier.Wait()
+			barrier2.Signal()
+		}
+		mutex.Signal()
+
+		//Block here again until barrier 2 is unlocked
+		barrier2.Wait()
+		barrier2.Signal()
 	}
-	mutex.Signal()
-
-	// Block here until everyone reached rendezvous and thread N signals the barrier
-	barrier.Wait()
-	// Thread N-i unlocks the barrier by decrementing the semaphore to -1 and waking up the N-i-1 thread.
-	// Thread N-i-1 will increment the barrier to 0 and signal the N-2 thread, until all threads pass.
-	// Note: Thread N will call Signal() two times, this will cause the semaphore to be decremented one extra
-	// The barrier will not reset after all threads have executed the critical part
-	barrier.Signal()
-	println(id, "critical after")
-
-	// Barrier reset logic here
-
 }
 
 func main() {
 
-	nThreads := 5
+	nThreads := 4
 	waitCount := nThreads
 	mutex := newSemaphore(1)
 	barrier := newSemaphore(0)
+	barrier2 := newSemaphore(1)
 
 	for i := 1; i <= nThreads; i++ {
-		go worker(i, &waitCount, mutex, barrier)
+		go worker(i, waitCount, &waitCount, mutex, barrier, barrier2)
 	}
 
-	<-time.After(time.Second * 3)
+	<-time.After(time.Second * 5)
 
-	//The barrier in it's current form is not reusable, after all threads finish executing the counter is -1
-	println(barrier.count)
 }
